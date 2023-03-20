cd98de5951f0448a7650a7dd78da120d28014536
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-03-20 22:45:49 +0200

    multi selsectors handled

--- src/cmponents/MyCounter.jsx
+++ src/cmponents/MyCounter.jsx
@@ -5,28 +5,60 @@ import { useDispatch, useSelector } from "react-redux"
 export default function MyCounter() {
   const dispatach = useDispatch();
 
-  const count = useSelector((state) => {
-    if (state.count < 1) return "no number";
-    return state.count;
-  });
+//   const count = useSelector((state) => {
+//     if (state.count < 1) return "no number";
+//     return state.count;
+//   });
 
-  const increase = () => {
-    const action = { type: "increase", payload:2 };
+//   const toggleState = useSelector(state=> state.showCount);
+
+/*---------------Global State better than multi selector--------------*/
+// const globalState = useSelector(state=>state)
+
+const {showCount,count} = useSelector(state=>state)
+
+
+/*---------------Handle less than zero--------------*/
+const hanldeCountValue = (val) =>  val < 1? "no number" : val;
+
+
+
+//   const increase = () => {
+//     const action = { type: "increase", payload:2 };
+//     dispatach(action)
+//   };
+
+//   const decrease = () => {
+//     const action = { type: "decrease" , payload:2};
+//     dispatach(action)
+
+// };
+
+/*---------------more Action Handler--------------*/
+
+const countHandler = (type, payload) => dispatach({type, payload});
+
+/*-------------Could Also be included in handle-----*/
+
+const toggleCounter = () => {
+    const action = {type: "toggleCounter"};
     dispatach(action)
-  };
+}
 
-  const decrease = () => {
-    const action = { type: "decrease" , payload:2};
-    dispatach(action)
-
-};
+// console.log(toggleState)
 
   return (
     <div>
       <h2>Hello Redux </h2>
-      <p className='fw-bold '>Count : {count}</p>
-      <button className="btn btn-success mx-3 px-3" onClick={increase}>Increase +</button>
-      <button className="btn btn-danger mx-3 px-3" onClick={decrease}>Decrease -</button>
+      {showCount && <>
+      <p className='fw-bold '>Count : {hanldeCountValue(count)}</p>
+      <div className="btns">
+      <button className="btn btn-success mx-3 px-3" onClick={()=> countHandler("increase", 2)}>Increase +</button>
+      <button className="btn btn-danger mx-3 px-3" onClick={() => countHandler("decrease", 1)}>Decrease -</button>
+      </div>
+      </>}
+      <button className="btn btn-warning my-3 mx-auto px-3 d-block" onClick={toggleCounter}>Show /Hide Count </button>
+      
     </div>
   );
 }
--- src/store/countReucer.js
+++ src/store/countReucer.js
@@ -1,4 +1,4 @@
-export const initailState ={ count: 5};
+export const initailState ={ count: 5, showCount: false};
 
 const countReducer = (state = initailState, action) => {
     switch(action.type) {
@@ -7,22 +7,12 @@ const countReducer = (state = initailState, action) => {
 
         case ("decrease" ):
         return {...state, count: state.count - action.payload}
+
+        case ("toggleCounter"):
+            return {...state, showCount: !state.showCount}
         
         default: state;
     }
-
-
-    // if(action.type === "increase"){
-    //     console.log("increase")
-    //     return {...state, count: state.count + action.payload}
-
-    // }
-
-    // if(action.type === "decrease"){
-    //     return {...state, count: state.count - action.payload}
-
-    // }
-    // console.log(action)
 
     return state;
 }
